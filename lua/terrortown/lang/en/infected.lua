134e632d2da24b5ef9a11dd1c5c1b342b5f1e948
uenetron <srb5467@gmail.com>
2021-11-06 17:36:10 +0000

    Update lua/terrortown/lang/en/infected.lua
    Co-authored-by: Histalek <16392835+Histalek@users.noreply.github.com>

--- lua/terrortown/lang/en/infected.lua
+++ lua/terrortown/lang/en/infected.lua
@@ -11,7 +11,7 @@ L["search_role_" .. INFECTED.abbr] = "This person was an Infected!"
 L["ev_win_" .. INFECTED.defaultTeam] = "The ill Infected won the round!"
 L["target_" .. INFECTED.name] = "Infected"
 L["ttt2_desc_" .. INFECTED.name] = [[The Infected needs to infect every player to win. They infect other players by killing them.
-If a player gets infected, the player becomes a zombie and is able to infect other players. This allows zombies to build up a whole army.
+If a player gets infected, they become a zombie and are able to infect other players. This allows the infected to build up a whole army.
 However, there is one thing you need to get in mind: If the host (the main infected player with a normal model) dies or disconnects, each player that the host directly infected will die.
 
 If there is a Jester, feel free to infect them.]]
